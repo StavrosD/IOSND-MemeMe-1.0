3e0e5ea189a5302fab162fa740714fa311cc84eb
Dimopoulos Stavros tou Athanasiou <sdim@sdim.gr>
2020-01-18 18:07:49 +0200

    Initial Commit

---
+++ IOSND-MemeMe-1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSND-MemeMe-1.0
+//
+//  Created by Dimopoulos Stavros tou Athanasiou on 18/1/20.
+//  Copyright © 2020 Dimopoulos Stavros tou Athanasiou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
